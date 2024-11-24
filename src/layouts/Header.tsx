bc4b55f3c5706b09e64cf8668bc9847bb0e81add
Gérald <cedricguidi735@gmail.com>
2024-11-24 01:35:52 +0100

    style fix

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -16,15 +16,16 @@ export default function Header() {
     <header className="bg-[#272B3B] dark:bg-[#101422] text-white">
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
+          {/* Logo for all screen sizes */}
           <motion.div
             initial={{ opacity: 0, x: -20 }}
             animate={{ opacity: 1, x: 0 }}
             transition={{ duration: 0.5 }}
-            className="flex justify-center items-center pb-1 w-full md:w-auto md:justify-start"
+            className="flex items-center"
           >
             <Link
               to="/"
-              className="flex gap-4 items-center justify-center text-2xl font-bold text-[#A8DCE7]"
+              className="hidden md:flex gap-4 items-center text-2xl font-bold text-[#A8DCE7]"
             >
               <img
                 src="/logo1.png"
@@ -34,6 +35,8 @@ export default function Header() {
               <p>Milesto</p>
             </Link>
           </motion.div>
+
+          {/* Navigation for medium and larger screens */}
           <nav className="hidden md:flex space-x-4">
             {navItems.map((item, index) => (
               <motion.div
@@ -51,7 +54,28 @@ export default function Header() {
               </motion.div>
             ))}
           </nav>
+
+          {/* ThemeSwitcher for medium and larger screens */}
           <div className="hidden md:flex items-center">
+            <ThemeSwitcher />
+          </div>
+
+          {/* Small screen layout */}
+          <div className="flex md:hidden justify-between items-center w-full">
+            {/* Logo on the left */}
+            <Link
+              to="/"
+              className="flex gap-4 items-center text-2xl font-bold text-[#A8DCE7]"
+            >
+             <img
+                src="/logo1.png"
+                alt="logo"
+                className="w-10 h-10 rounded-full"
+              />
+              <p>Milesto</p>
+            </Link>
+
+            {/* ThemeSwitcher on the right */}
             <ThemeSwitcher />
           </div>
         </div>
