302ecfcc57b9f66ff13642b31b5967cd030a7709
Gérald <cedricguidi735@gmail.com>
2024-12-31 00:15:17 +0100

    login problem and Date problem resolved

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+import tailwindcssAnimate from "tailwindcss-animate";
+
 export default {
   darkMode: ["class"],
   content: ["./src/**/*.{html,ts,tsx}"],
@@ -53,5 +55,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [tailwindcssAnimate],
 };
